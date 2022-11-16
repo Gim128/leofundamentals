0179f200d9331338ca774ca128d27bc0f60dbb73
ican-graphics-01 <lankaican@gmail.com>
2022-11-16 14:51:19 +0530

    init project & screens

---
+++ components/title.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Title = () => {
+  return (
+    <View>
+      <Text>Leo Quizzler</Text>
+    </View>
+  );
+};
+
+export default Title;
+
+const styles = StyleSheet.create({});
